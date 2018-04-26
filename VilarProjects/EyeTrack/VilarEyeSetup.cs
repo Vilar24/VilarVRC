8eca2b1a1d6b72a5fcfb3296e243fa9e3ddffc6b
Unknown <void24@gmail.com>
2018-04-25 23:37:26 -0500

    Fixed compiler error

--- VilarProjects/EyeTrack/VilarEyeSetup.cs
+++ VilarProjects/EyeTrack/VilarEyeSetup.cs
@@ -1,4 +1,4 @@
-﻿//#if UNITY_EDITOR
+﻿#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -84,4 +84,4 @@ public class VilarEyeSetup : ScriptableWizard {
 	}
 
 }
-//#endif
+#endif
